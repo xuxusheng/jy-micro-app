1169491fb8c9e658b6bf3a0bab913c617ce83aab
许盛 <20691718@qq.com>
2024-10-16 15:09:47 +0800

    feat: 加入重定向逻辑

--- apps/frontend/src/routers/index.tsx
+++ apps/frontend/src/routers/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter, Navigate } from 'react-router-dom'
 import { lazy } from 'react'
 import { Demo01Page } from '../pages/Demo01'
 
@@ -7,10 +7,10 @@ const About = lazy(() => import('../pages/About'))
 
 const router = createBrowserRouter(
   [
-    {
-      path: '/',
-      element: <Home />
-    },
+    // {
+    //   path: '/',
+    //   element: <Home />
+    // },
     {
       path: '/about',
       element: <About />
@@ -18,6 +18,11 @@ const router = createBrowserRouter(
     {
       path: 'demo01',
       element: <Demo01Page />
+    },
+    {
+      // 重定向
+      path: '*',
+      element: <Navigate to="/demo01" />
     }
   ],
   { basename: window.__POWERED_BY_QIANKUN__ ? '/micro-app/3' : '/' }
