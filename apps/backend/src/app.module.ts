439b4856c7db3698d103396c58dbe5a27cd61a47
许盛 <20691718@qq.com>
2024-10-31 15:20:58 +0800

    chore: 加入 docker-compose 文件

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import * as path from 'node:path';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
+  controllers: [AppController],
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: path.join(__dirname, '..', 'frontend'),
       exclude: ['/api/(.*)'],
+      rootPath: path.join(__dirname, '..', 'frontend'),
     }),
   ],
-  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
