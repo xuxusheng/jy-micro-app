28340567b27c99c5ef8d65d27e78f9ee2f038e71
许盛 <20691718@qq.com>
2025-01-08 18:40:32 +0800

    feat: 加入 ShaoshanService

---
+++ apps/backend/src/module/shared/service/shaoshan.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common'
+
+
+// 调用 shaoshan 站 API
+@Injectable()
+export class ShaoshanService {}
--- apps/backend/src/module/shared/shared.module.ts
+++ apps/backend/src/module/shared/shared.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
+import { ShaoshanService } from './service/shaoshan.service'
 // import { PrismaService } from './service/prisma.service'
 // import { RunnerService } from './service/runner.service'
 
 @Module({
-  imports: []
+  imports: [],
+  providers: [ShaoshanService]
   // providers: [RunnerService, PrismaService],
   // exports: [RunnerService, PrismaService]
 })
