5e53edda29b0b2ff55fb30a485cda761e4262ddc
许盛 <20691718@qq.com>
2025-01-13 20:21:58 +0800

    feat: 加入历史数据测试接口

--- apps/backend/src/module/shaoshan/shaoshan.controller.ts
+++ apps/backend/src/module/shaoshan/shaoshan.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Query } from '@nestjs/common'
 import { ShaoshanService } from './shaoshan.service'
 import { ShaoshanExternalApiService } from '../shared/service/shaoshan-external-api.service'
+import dayjs from 'dayjs'
 
 @Controller('api/shaoshan')
 export class ShaoshanController {
@@ -15,11 +16,25 @@ export class ShaoshanController {
     return this.shaoshanExternalApiService.getToken()
   }
 
-  // 测试通过 key 获取 value 的能力
-  @Get('test/get-value-by-key')
-  testGetValueByKey(@Query('key') key: number) {
-    return this.shaoshanExternalApiService.getValuesByKeys([
-      key || 4222126589739011
+  // 测试获取实时数据功能
+  @Get('test/get-realtime-data')
+  testGetValueByKey(@Query('key') key: string) {
+    return this.shaoshanExternalApiService.getRealtimeData([
+      key || '4222126589739011'
     ])
+  }
+
+  // 测试获取历史数据的功能
+  @Get('test/get-history-data')
+  testGetHistoryData(@Query('key') key: string) {
+    const startTime = dayjs().subtract(1, 'hour')
+    const endTime = dayjs()
+
+    return this.shaoshanExternalApiService.getHistoryData({
+      keys: [key || '4222126589739011'],
+      startTime,
+      endTime,
+      interval: 5
+    })
   }
 }
--- apps/backend/src/module/shared/service/shaoshan-external-api.service.ts
+++ apps/backend/src/module/shared/service/shaoshan-external-api.service.ts
@@ -3,13 +3,14 @@ import axios from 'axios'
 import { shaoshanConfig } from '../../core/config/shaoshan.config'
 import { ConfigType } from '@nestjs/config'
 import * as qs from 'qs'
+import { Dayjs } from 'dayjs' // 调用 shaoshan 站 API
 
 // 调用 shaoshan 站 API
 @Injectable()
 export class ShaoshanExternalApiService {
   constructor(
     @Inject(shaoshanConfig.KEY)
-    private shaoshanConf: ConfigType<typeof shaoshanConfig>
+    private readonly shaoshanConf: ConfigType<typeof shaoshanConfig>
   ) {}
 
   // 获取 token
@@ -40,8 +41,8 @@ export class ShaoshanExternalApiService {
     return res.data.access_token
   }
 
-  // 批量通过 key 值获取 value
-  getValuesByKeys = async (keys: number[]) => {
+  // 批量查询实时数据
+  getRealtimeData = async (keys: string[]) => {
     const { url, hwID, hwAppKey, clientId } = this.shaoshanConf
 
     const token = await this.getToken()
@@ -54,8 +55,8 @@ export class ShaoshanExternalApiService {
 
     const res = await axios.request<{
       id: number
-      code: number
-      message: string
+      code: number // 2023
+      message: string // 获取实时信号值成功
       body: {
         values: Array<{
           fresh_time: string // 2024-12-10 22:36:11.331
@@ -70,11 +71,69 @@ export class ShaoshanExternalApiService {
       method: 'POST',
       headers,
       data: {
-        id: 1,
+        // 输入多少返回多少，采用 32 位有符号整数，最大可表示的正整数为 2,147,483,647，再大就溢出变为负数了
+        // 可以 random 一个随机正整数，然后当做接口标识，方便调试
+        id: Math.floor(Math.random() * 2147483647),
         client_id: clientId,
         body: {
           datatype: 'analog',
           keys
+        }
+      }
+    })
+
+    return res.data.body.values
+  }
+
+  // 批量查询历史数据
+  // 公共组件支持应用按需查询指定一组遥测信号的历史数据。单次查询数据返回总量不应超过10万条，超过一定量时，返回调用失败，提示采用分批多次取数据方式获取数据，以防网络拥堵。
+  getHistoryData = async (data: {
+    keys: string[] // 信号索引键，需查询的信号列表
+    startTime: Dayjs // 开始时间 yyyy-MM-dd HH:mm:ss.SSS
+    endTime: Dayjs // 结束时间 yyyy-MM-dd HH:mm:ss.SSS
+    interval: number // 取值间隔，如：5，表示五分钟取一个值
+    count?: number // 是否查询统计值 0：不查询统计值 1：查询统计值
+  }) => {
+    const { keys, startTime, endTime, count = 0, interval } = data
+    const { url, hwID, hwAppKey, clientId } = this.shaoshanConf
+
+    const token = await this.getToken()
+
+    const headers = {
+      'X-HW-ID': hwID,
+      'X-HW-APPKEY': hwAppKey,
+      Authorization: `Bearer ${token}`
+    }
+
+    const res = await axios.request<{
+      id: number
+      code: number // 2026
+      message: string // ""
+      body: {
+        values: Array<{
+          key: string
+          vals: Array<{
+            time: string // 2024-12-10 22:36:11.331
+            value: number
+          }>
+        }>
+      }
+    }>({
+      url: `${url}/v1/cs/hisdata-service/data/history`,
+      method: 'POST',
+      headers,
+      data: {
+        // 输入多少返回多少，采用 32 位有符号整数，最大可表示的正整数为 2,147,483,647，再大就溢出变为负数了
+        // 可以 random 一个随机正整数，然后当做接口标识，方便调试
+        id: Math.floor(Math.random() * 2147483647),
+        client_id: clientId,
+        body: {
+          datatype: 'analog',
+          keys,
+          startTime: startTime.format('YYYY-MM-DD HH:mm:ss.SSS'),
+          endTime: endTime.format('YYYY-MM-DD HH:mm:ss.SSS'),
+          interval,
+          count
         }
       }
     })
